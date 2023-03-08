7611e6726785c5133911929fb32facc108da93cf
twqueen <tiffanyytsai@gmail.com>
2023-03-07 23:11:36 -0800

    added server and routes js

---
+++ server.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const path = require('path');
+const api = require('./routes/apiRoutes.js')
+
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use('/api/notes', api);
+app.use(express.static('public'));
+
+// get route to homepage
+app.get('/', (req, res) =>
+    res.sendFile(path.join(__dirname, '/public/index.html'))
+);
+
+// get route to notes page
+app.get('/notes', (req, res) =>
+    res.sendFile(path.join(__dirname, '/public/notes.html'))
+);
+
+app.listen(PORT, () =>
+    console.log(`App listening at ${PORT}`)
+);
